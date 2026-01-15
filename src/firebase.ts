44527ba09e1aa08eab59722fff76b0bdfcfc9a7f
wild-butterfly <askinyagmurkose@outlook.com>
2026-01-15 19:55:45 +1100

    Mask Firebase configuration values
    Replaced sensitive Firebase configuration values with placeholders.

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBzCyZj58fS2U0_CGPEk6p1dNmXLJwkF9o",
-  authDomain: "honeycomb-au.firebaseapp.com",
-  projectId: "honeycomb-au",
-  storageBucket: "honeycomb-au.appspot.com",
-  messagingSenderId: "758798273736",
-  appId: "1:758798273736:web:1bc38f393aef1ff3a6351d",
-  measurementId: "G-VXJ5074GK3",
+  apiKey: "***************",
+  authDomain: "***************",
+  projectId: "***************",
+  storageBucket: "***************",
+  messagingSenderId: "***************,
+  appId: "***************",
+  measurementId: "***************",
 };
 
 const app = initializeApp(firebaseConfig);
